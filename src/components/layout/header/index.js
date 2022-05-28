f5bccff55008f4baabb0364d1a0709f150442b03
Hubert Kulinski <hubert.kulinski9@gmail.com>
2022-05-28 17:36:50 +0200

    add function which is responsible for the return to the MainPageBeers after click on logo

--- src/components/layout/header/index.js
+++ src/components/layout/header/index.js
@@ -1,9 +1,12 @@
 import logoImage from 'assets/images/logo.png';
 import classes from './Header.module.css';
+import { useHistory } from 'react-router-dom';
 
 const Header = () => {
+    const history = useHistory()
 
-    const scrollToTop = () => {
+    const redirectToMainPage = () => {
+        history.push("/beers")
         window.scroll({
             top: 0,
             behavior: 'smooth',
@@ -13,12 +16,12 @@ const Header = () => {
     return (
         <div className={classes.header}>
             <img className={classes['main-img']}
-                onClick={scrollToTop}
+                onClick={redirectToMainPage}
                 src={logoImage}
                 alt='beer logo'
             />
             <span className={classes.logoDescription}
-                onClick={scrollToTop}
+                onClick={redirectToMainPage}
             >
                 globo's beers
             </span>
